275b4b3af63488a6e2d123c5311eaf7277e1b134
Raphael Melo <raphaelgmelo@gmail.com>
2016-02-02 09:26:07 -0200

    prod config

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'        : process.env.TWITTER_CONSUMER_KEY,
+        'consumerSecret'     : process.env.TWITTER_CONSUMER_SECRET,
+        'accessTokenKey'     : process.env.TWITTER_TOKEN_KEY,
+        'accessTokenSecret'  : process.env.TWITTER_TOKEN_SECRET,
+        'callbackURL'        : process.env.TWITTER_CALLBACK_URL,
+    },
+
+};
