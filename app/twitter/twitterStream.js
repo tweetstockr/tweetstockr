5db840b44a93c6d6d895771253d7ef84ae5e2dea
Raphael Melo <raphaelgmelo@gmail.com>
2016-02-21 17:36:25 -0300

    revert changes

--- app/twitter/twitterStream.js
+++ app/twitter/twitterStream.js
@@ -23,7 +23,7 @@ module.exports = function(server){
 
   var io = require('socket.io').listen(server);
 
-  // io.set('origins', configGeneral.allowedOriginA + ' ' + configGeneral.allowedOriginB);
+  io.set('origins', configGeneral.allowedOriginA + ' ' + configGeneral.allowedOriginB);
 
   io.on('connection', function (socket) {
       // console.log('User connected!');
