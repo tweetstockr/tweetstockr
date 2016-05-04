bf1e8102cb8b4df5abc3576eae719a2fe8868159
Raphael Melo <raphaelgmelo@gmail.com>
2016-05-04 11:41:49 -0300

    market tabs

--- public/scripts/controllers/marketController.js
+++ public/scripts/controllers/marketController.js
@@ -59,28 +59,28 @@
 
           for (var i = 0; i < formattedStocks.length; i++) {
             var stock = formattedStocks[i];
-            var dataLenght = stock.history.length;
-
-            if (stock.price > 0 && dataLenght > 1) {
-              if (stock.history[1].price > 0) {
-                var variationNumber = (( stock.price / stock.history[1].price ) - 1) * 100;
-                stock.variation = Math.round(variationNumber).toFixed(0) + '%';
-                stock.lastMove = (variationNumber < 0) ? 'danger' : 'success';
-                stock.icon = (variationNumber < 0) ? 'fa-caret-down' : 'fa-caret-up';
-              }
-            }
+            // var dataLenght = stock.history.length;
+            //
+            // if (stock.price > 0 && dataLenght > 1) {
+            //   if (stock.history[1].price > 0) {
+            //     var variationNumber = (( stock.price / stock.history[1].price ) - 1) * 100;
+            //     stock.variation = Math.round(variationNumber).toFixed(0) + '%';
+            //     stock.lastMove = (variationNumber < 0) ? 'danger' : 'success';
+            //     stock.icon = (variationNumber < 0) ? 'fa-caret-down' : 'fa-caret-up';
+            //   }
+            // }
 
             var chartData = {};
             chartData.labels = [];
             chartData.series = [[]];
 
-            for (var j = dataLenght-1; j >= 0; j--) {
-              var time = new Date(stock.history[j].created_at);
-              var label = time.getHours() + ':' + time.getMinutes();
-
-              chartData.series[0].push(stock.history[j].price);
-              chartData.labels.push(label);
-            }
+            // for (var j = dataLenght-1; j >= 0; j--) {
+            //   var time = new Date(stock.history[j].created_at);
+            //   var label = time.getHours() + ':' + time.getMinutes();
+            //
+            //   chartData.series[0].push(stock.history[j].price);
+            //   chartData.labels.push(label);
+            // }
 
             stock.chartData = chartData;
           }
@@ -109,37 +109,14 @@
       showArea: true
     }
 
-    $scope.tabs = [{
-        title: 'Shares'
-      , url: 'components/shares.html'
-      , icon: 'icons/shares-icon.html'
-    }, {
-        title: 'Portfolio'
-      , url: 'components/portfolio.html'
-      , icon: 'icons/portfolio-icon.html'
-    }];
-
-    $scope.currentTab = 'components/shares.html';
+    $scope.currentTab = 'SHARES';
 
     $scope.onClickTab = function (tab) {
-      if(tab.url === 'components/shares.html') {
-        $scope.currentTab = tab.url;
-
-        $scope.isActiveTab = function (tabUrl) {
-          return tabUrl === $scope.currentTab;
-        };
-
-      } else if(tab.url === 'components/portfolio.html') {
-        $scope.currentTab = tab.url;
-
-        $scope.isActiveTab = function (tabUrl) {
-          return tabUrl === $scope.currentTab;
-        };
-      }
+      $scope.currentTab = tab;
     };
 
-    $scope.isActiveTab = function (tabUrl) {
-      return tabUrl === $scope.currentTab;
+    $scope.isActiveTab = function (tab) {
+      return $scope.currentTab === tab;
     };
 
     $scope.sellShare = function(share) {
@@ -181,18 +158,18 @@
 
           for (var i = 0; i < $scope.portfolio.length; i++) {
             var portfolio = $scope.portfolio[i];
-            var dataLenght = portfolio.history.length;
+            // var dataLenght = portfolio.history.length;
             var chartData = {};
             chartData.labels = [];
             chartData.series = [[]];
-
-            for (var j = dataLenght-1; j >= 0; j--) {
-              var time = new Date(portfolio.history[j].created_at);
-              var label = time.getHours() + ':' + time.getMinutes();
-
-              chartData.series[0].push(portfolio.history[j].price);
-              chartData.labels.push(label);
-            }
+            //
+            // for (var j = dataLenght-1; j >= 0; j--) {
+            //   var time = new Date(portfolio.history[j].created_at);
+            //   var label = time.getHours() + ':' + time.getMinutes();
+            //
+            //   chartData.series[0].push(portfolio.history[j].price);
+            //   chartData.labels.push(label);
+            // }
 
             portfolio.chartData = chartData;
           }
